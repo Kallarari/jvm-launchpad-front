9fd43d583577450ea57aeaf129d6e4f58334d76b
Mariana <mariana.iwaivk@gmail.com>
2025-12-20 11:55:01 -0300

    feat(router): add new route in rotas-squad-4.tsx

--- src/router/rotas-squad-4.tsx
+++ src/router/rotas-squad-4.tsx
@@ -1,9 +1,10 @@
 import { Route } from "react-router-dom";
+import { SquadConfigPage } from "../pages/SquadConfigPage";
 
 export const Squad4Routes = () => {
   return (
     <>
-      <Route path="/Squad" element={<h1>Squad exemple page</h1>} />
+      <Route path="/squad-config" element={<SquadConfigPage />} />
       <Route path="/Squad-exemple" element={<h1>Squad exemple page</h1>} />
     </>
   );
