5ae85469b6a4968b0e967d9e86dda886882b5400
KallylC <kallylcantalice1@hotmail.com>
2025-12-23 18:56:07 -0300

    add: arquivos login-page

--- src/router/rotas-squad-1.tsx
+++ src/router/rotas-squad-1.tsx
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import LandingPage from "../pages/landing-page";
+// import LoginPage from "../pages/login-page";
 
 export const Squad1Routes = () => {
   return (
     <>
       <Route index element={<LandingPage />} />
+      { /* <Route path="/Login" element={<LoginPage />} /> */ }
       <Route path="/Squad" element={<h1>Squad exemple page</h1>} />
       <Route path="/Squad-exemple" element={<h1>Squad exemple page</h1>} />
     </>
