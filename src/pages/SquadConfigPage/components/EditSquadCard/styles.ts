52df3cea963aacf1db814ea3623f2cec0da58c87
Mariana <mariana.iwaivk@gmail.com>
2025-12-26 09:02:15 -0300

    feat(edit-squad-card): add EditSquadCard component

---
+++ src/pages/SquadConfigPage/components/EditSquadCard/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { BaseCard } from "../../../../squad-4-src/components/BaseCard";
+
+export const EditCard = styled(BaseCard)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
