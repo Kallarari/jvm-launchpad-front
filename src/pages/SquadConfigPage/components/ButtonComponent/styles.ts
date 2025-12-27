cf9a0eaa3d127e28e98193bd560c72971d6844a7
Mariana <mariana.iwaivk@gmail.com>
2025-12-27 02:29:28 -0300

    feat(button-component): make font sizes flexible and add responsive sizing

--- src/pages/SquadConfigPage/components/ButtonComponent/styles.ts
+++ src/pages/SquadConfigPage/components/ButtonComponent/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import "../../../../squad-4-src/styles/fonts.css"
+import "../../../../squad-4-src/styles/fonts.css";
 import { Icon } from "@iconify/react";
 import { theme } from "../../../../infrastructure";
 import type { ButtonSize } from "./interface";
@@ -10,36 +10,34 @@ interface StyledButtonProps {
 
 export const StyledIcon = styled(Icon)`
   width: 53px;
-  height: 53px
+  height: 53px;
+
+  @media (max-height: 833px) {
+    width: 45px;
+    height: 45px;
+  }
 `;
 
 export const StyledButton = styled.button<StyledButtonProps>`
-  
   background-color: ${theme.colors.danger};
   color: ${theme.colors.white};
-
   min-width: ${({ size }) => (size === "sm" ? "161px" : "223px")};
   width: auto;
   height: ${({ size }) => (size === "sm" ? "46px" : "74px")};
-
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 0 30px;
-
   margin: auto;
   cursor: pointer;
-
   border: none;
   border-radius: ${theme.borderRadius.lg};
-
-  font-family: 'Goldman', sans-serif;
-  font-size: ${({ size }) => (size === "sm" ? "16px" : "20px")}; 
+  font-family: "Goldman", sans-serif;
+  font-size: ${({ size }) => (size === "sm" ? "16px" : "20px")};
   font-weight: 400;
 
-  * {
-    pointer-events: none;
-    color: inherit;
-    line-height: 1;
+  @media (max-height: 833px) {
+    height: ${({ size }) => (size === "sm" ? "43px" : "64px")};
+    font-size: ${({ size }) => (size === "sm" ? "14px" : "18px")};
   }
 `;
