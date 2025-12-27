3f46bf32d0f32cbbedfc53e9fe59c2f1c50989dd
Luiz <fellipevantine12@gmail.com>
2025-12-27 12:15:52 -0300

    style(add-points-button) add component styles

--- src/pages/PointsManagementPage/components/AddPointsButton/styles.ts
+++ src/pages/PointsManagementPage/components/AddPointsButton/styles.ts
@@ -1,6 +1,18 @@
 import styled from "styled-components";
-//import "../../../../squad-4-src/styles/fonts.css"
+import "../../../../squad-4-src/styles/fonts.css";
 
-export const Teste = styled.h1`
-  color:  pink;
+export const AddPointsStyled = styled.button`
+  min-width: 73px;
+  height: 42px;
+
+  background-color: #101010;
+  color: #fff;
+
+  font-family: "Goldman", sans-serif;
+  font-weight: 400px;
+  font-size: 24px;
+
+  cursor: pointer;
+
+  border-radius: 10px;
 `;
