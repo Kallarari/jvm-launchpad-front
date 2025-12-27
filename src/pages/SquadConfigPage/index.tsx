8bde554ae0400b76b8f99974d489b8491b701dc0
Mariana <mariana.iwaivk@gmail.com>
2025-12-27 02:31:50 -0300

    feat(squad-config-page): add initial test version to open modal

--- src/pages/SquadConfigPage/index.tsx
+++ src/pages/SquadConfigPage/index.tsx
@@ -1,12 +1,49 @@
-import { ExemploComponente } from "./components/ExemploComponente"
-/* Pode por seu componente ali em return para que vc consiga olhar na pagina como ele fica e edita-lo. 
-   Nao pode commitar o seu componente aqui, use so para editar e dps apague desse arquivo
-*/
+import { useEffect, useState } from "react";
+import { getSquadsMock } from "../../squad-4-src/mocks/squads.mock";
+import type { Squad } from "../../squad-4-src/interfaces/squad";
+import { ButtonComponent } from "./components/ButtonComponent";
+import { SquadEditionModal } from "./components/SquadEditionModal";
+
 export const SquadConfigPage = () => {
-   return (
-      <>
-         <ExemploComponente titulo="oi" />
-         <ExemploComponente titulo="tchau" />
-      </>
-   )
-}
+  const [squads, setSquads] = useState<Squad[]>([]);
+  const [selectedSquad, setSelectedSquad] = useState<Squad | null>(null);
+
+  useEffect(() => {
+    getSquadsMock().then(setSquads);
+  }, []);
+
+  const updateSquad = (updatedSquad: Squad) => {
+    setSquads((prev) =>
+      prev.map((squad) => (squad.id === updatedSquad.id ? updatedSquad : squad))
+    );
+  };
+
+  return (
+    <>
+      {!selectedSquad && (
+        <>
+          <ul>
+            {squads.map((squad) => (
+              <li key={squad.id} style={{ marginBottom: "1rem" }}>
+                {squad.name}
+                <ButtonComponent
+                  label={`Editar ${squad.name}`}
+                  size="sm"
+                  onClick={() => setSelectedSquad(squad)}
+                />
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      {selectedSquad && (
+        <SquadEditionModal
+          squad={selectedSquad}
+          onClose={() => setSelectedSquad(null)}
+          onSave={updateSquad}
+        />
+      )}
+    </>
+  );
+};
