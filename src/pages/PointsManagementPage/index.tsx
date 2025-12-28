71477f830a153fddccfe8b21f498e9493e368802
Luiz <fellipevantine12@gmail.com>
2025-12-28 18:21:20 -0300

    refactor(score-management-view): render members list using filter and map and remove repeted JSX

--- src/pages/PointsManagementPage/index.tsx
+++ src/pages/PointsManagementPage/index.tsx
@@ -1,9 +1,16 @@
-import { AddPointsButton } from "./components/AddPointsButton";
+import { ScoreManagementView } from "./components/ScoreManagementView";
 
 /* Pode por seu componente ali em return para que vc consiga olhar na pagina como ele fica e edita-lo. 
    Nao pode commitar o seu componente aqui, use so para editar e dps apague desse arquivo
 */
 
 export const PointsManagementPage = () => {
-  return <AddPointsButton />;
+  return (
+    <>
+      <ScoreManagementView
+        MemberName="MemberName"
+        MemberPoints="MemberPoints"
+      />
+    </>
+  );
 };
