ebfd4e0f85a73522fbb628d4a03c2383c3a31040
Luiz <fellipevantine12@gmail.com>
2025-12-28 18:32:38 -0300

    refactor(score-management-view): remove duplicated JSX and iterate members list

--- src/pages/PointsManagementPage/components/ScoreManagementView/index.tsx
+++ src/pages/PointsManagementPage/components/ScoreManagementView/index.tsx
@@ -3,7 +3,9 @@ import { AddPointsButton } from "../AddPointsButton";
 import { ScoreManagementStyled } from "./style";
 import { PointsButtonWrap } from "./style";
 import { ScoreManagementWrap } from "./style";
-// import { usersMock } from "../../../../squad-4-src/mocks/users.mock";
+import { MemberNameStyled } from "./style";
+import { MemberPointsStyled } from "./style";
+import { usersMock } from "../../../../squad-4-src/mocks/users.mock";
 
 export const ScoreManagementView = ({
   MemberName,
@@ -12,35 +14,25 @@ export const ScoreManagementView = ({
   return (
     <>
       <ScoreManagementWrap>
-        <ScoreManagementStyled>
-          {MemberName && <div>{(MemberName = "Mariana")}</div>}
-          {MemberPoints && <div>{(MemberPoints = "1000 Pontos")}</div>}
-          <PointsButtonWrap>
-            {<AddPointsButton label="+10" />}
-            {<AddPointsButton label="+5" />}
-            {<AddPointsButton label="+2" />}
-          </PointsButtonWrap>
-        </ScoreManagementStyled>
-
-        <ScoreManagementStyled>
-          {MemberName && <div>{(MemberName = "Rafael")}</div>}
-          {MemberPoints && <div>{(MemberPoints = "1000 Pontos")}</div>}
-          <PointsButtonWrap>
-            {<AddPointsButton label="+10" />}
-            {<AddPointsButton label="+5" />}
-            {<AddPointsButton label="+2" />}
-          </PointsButtonWrap>
-        </ScoreManagementStyled>
-
-        <ScoreManagementStyled>
-          {MemberName && <div>{(MemberName = "Vantttine")}</div>}
-          {MemberPoints && <div>{(MemberPoints = "1000 Pontos")}</div>}
-          <PointsButtonWrap>
-            {<AddPointsButton label="+10" />}
-            {<AddPointsButton label="+5" />}
-            {<AddPointsButton label="+2" />}
-          </PointsButtonWrap>
-        </ScoreManagementStyled>
+        {usersMock
+          .filter((user) => user.role === "member" || user.role === "leader")
+          .map((user) => (
+            <ScoreManagementStyled key={user.id}>
+              {MemberName && (
+                <MemberNameStyled>{user.name.split(" ")[0]}</MemberNameStyled>
+              )}
+              {MemberPoints && (
+                <MemberPointsStyled key={user.points}>
+                  {user.points}
+                </MemberPointsStyled>
+              )}
+              <PointsButtonWrap>
+                {<AddPointsButton label="+10" />}
+                {<AddPointsButton label="+5" />}
+                {<AddPointsButton label="+2" />}
+              </PointsButtonWrap>
+            </ScoreManagementStyled>
+          ))}
       </ScoreManagementWrap>
     </>
   );
