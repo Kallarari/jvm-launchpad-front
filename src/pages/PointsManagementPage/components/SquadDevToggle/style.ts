cd16faf46d572cd248ad1f893cdb9d6e8d30e62b
Luiz <fellipevantine12@gmail.com>
2025-12-30 00:58:26 -0300

    style(squad-dev-toggle): initial component style

---
+++ src/pages/PointsManagementPage/components/SquadDevToggle/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import "../../../../squad-4-src/styles/fonts.css";
+
+export const ToggleButton = styled.span`
+  width: 200px;
+  height: 74px;
+
+  font-family: "Goldman", sans-serif;
+  font-weight: 400;
+  font-size: 20px;
+
+  color: #fff;
+  display: flex;
+`;
+export const DevButton = styled.span`
+  background-color: rgba(230, 57, 70, 1);
+`;
+export const SquadButton = styled.span`
+  border: 2px solid #fff;
+  background-color: rgba(16, 16, 16, 1);
+`;
