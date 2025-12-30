7396be787404a0ea07e2b2692a11a67a99d749c0
Luiz <fellipevantine12@gmail.com>
2025-12-30 01:54:42 -0300

    style(squad-dev-toggle): fix active button border and background

--- src/pages/PointsManagementPage/components/SquadDevToggle/style.ts
+++ src/pages/PointsManagementPage/components/SquadDevToggle/style.ts
@@ -1,21 +1,45 @@
-import styled from "styled-components";
 import "../../../../squad-4-src/styles/fonts.css";
+import styled from "styled-components";
 
-export const ToggleButton = styled.span`
+interface ButtonProps {
+  $active: boolean;
+}
+
+export const ToggleButton = styled.div`
   width: 200px;
   height: 74px;
+  min-width: 200px;
+  min-height: 74px;
 
-  font-family: "Goldman", sans-serif;
-  font-weight: 400;
-  font-size: 20px;
-
-  color: #fff;
   display: flex;
 `;
-export const DevButton = styled.span`
-  background-color: rgba(230, 57, 70, 1);
+
+// export const BackgroundTester = styled.button`
+//   background-color: #fff;
+// `;
+export const BaseButton = styled.button<ButtonProps>`
+  flex: 1 0 50%;
+  box-sizing: border-box;
+
+  border: ${({ $active }) =>
+    $active ? "2px solid #fff" : "2px solid transparent"};
+
+  background-color: ${({ $active }) =>
+    $active ? "rgba(230, 57, 70, 1)" : "rgba(16, 16, 16, 1)"};
+
+  color: #fff;
+  font-size: 20px;
+  font-family: "Goldman", sans-serif;
+  cursor: pointer;
+  white-space: nowrap;
+
+  transition: background-color 0.25s ease, border-color 0.25s ease;
 `;
-export const SquadButton = styled.span`
-  border: 2px solid #fff;
-  background-color: rgba(16, 16, 16, 1);
+
+export const DevButton = styled(BaseButton)`
+  border-radius: 0px 10px 10px 0px;
+`;
+
+export const SquadButton = styled(BaseButton)`
+  border-radius: 10px 0px 0px 10px;
 `;
