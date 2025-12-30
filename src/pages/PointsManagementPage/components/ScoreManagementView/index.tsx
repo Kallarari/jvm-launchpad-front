8a19762527766db426a9b35305e19e93404f7630
Luiz <fellipevantine12@gmail.com>
2025-12-30 00:09:29 -0300

    feat(score-management-view): show squad number for leaders

--- src/pages/PointsManagementPage/components/ScoreManagementView/index.tsx
+++ src/pages/PointsManagementPage/components/ScoreManagementView/index.tsx
@@ -1,39 +1,63 @@
+import { useEffect, useState } from "react";
+
 import type { ScoreManagementProps } from "./interface";
+import type { Squad } from "../../../../squad-4-src/interfaces/squad";
+
 import { AddPointsButton } from "../AddPointsButton";
-import { ScoreManagementStyled } from "./style";
-import { PointsButtonWrap } from "./style";
-import { ScoreManagementWrap } from "./style";
-import { MemberNameStyled } from "./style";
-import { MemberPointsStyled } from "./style";
+import { getSquadsMock } from "../../../../squad-4-src/mocks/squads.mock";
 import { usersMock } from "../../../../squad-4-src/mocks/users.mock";
+
+import {
+  ScoreManagementStyled,
+  PointsButtonWrap,
+  ScoreManagementWrap,
+  MemberNameStyled,
+  MemberPointsStyled,
+  LeaderText,
+} from "./style";
 
 export const ScoreManagementView = ({
   MemberName,
   MemberPoints,
 }: ScoreManagementProps) => {
+  const [squads, setSquads] = useState<Squad[]>([]);
+
+  useEffect(() => {
+    getSquadsMock().then(setSquads);
+  }, []);
+
   return (
-    <>
-      <ScoreManagementWrap>
-        {usersMock
-          .filter((user) => user.role === "member" || user.role === "leader")
-          .map((user) => (
+    <ScoreManagementWrap>
+      {usersMock
+        .filter((user) => user.role === "member" || user.role === "leader")
+        .map((user) => {
+          const squad = squads.find((squad) =>
+            squad.members.some((member) => member.id === user.id)
+          );
+
+          return (
             <ScoreManagementStyled key={user.id}>
               {MemberName && (
-                <MemberNameStyled>{user.name.split(" ")[0]}</MemberNameStyled>
+                <MemberNameStyled>
+                  {user.name.split(" ")[0]}{" "}
+                  {user.role === "leader" && squad && (
+                    <LeaderText>líder squad {squad.id}</LeaderText>
+                  )}
+                </MemberNameStyled>
               )}
+
               {MemberPoints && (
-                <MemberPointsStyled key={user.points}>
-                  {user.points}
-                </MemberPointsStyled>
+                <MemberPointsStyled>{user.points}</MemberPointsStyled>
               )}
+
               <PointsButtonWrap>
-                {<AddPointsButton label="+10" />}
-                {<AddPointsButton label="+5" />}
-                {<AddPointsButton label="+2" />}
+                <AddPointsButton label="+10" />
+                <AddPointsButton label="+5" />
+                <AddPointsButton label="+2" />
               </PointsButtonWrap>
             </ScoreManagementStyled>
-          ))}
-      </ScoreManagementWrap>
-    </>
+          );
+        })}
+    </ScoreManagementWrap>
   );
 };
