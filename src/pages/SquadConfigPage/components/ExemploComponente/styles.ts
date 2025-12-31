53c29e0ace8655e022c458c194ece6119b63b03a
Mariana <mariana.iwaivk@gmail.com>
2025-12-31 08:19:51 -0300

    chore(squad-config-page): remove unused example component files

--- src/pages/SquadConfigPage/components/ExemploComponente/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { theme } from '../../../../infrastructure';
-
-export const ComponentTitle = styled.h1`
-  font-size: ${theme.fontSizes['2xl']};
-  font-weight: 600;
-  color: ${theme.colors.primary};
-`
