b3e5d1c8ed2464e5f24b5dcffa1dd13143b0d2e5
Mariana <mariana.iwaivk@gmail.com>
2025-12-31 10:32:55 -0300

    fix(search-bar): add id and htmlFor to improve accessibility

--- src/pages/PointsManagementPage/components/SearchBar/index.tsx
+++ src/pages/PointsManagementPage/components/SearchBar/index.tsx
@@ -8,11 +8,19 @@ import {
 } from "./styles";
 
 export const SearchBar = ({ value, onChange }: SearchBarProps) => {
+  const inputId = "search-name";
+
   return (
     <SearchBarContainer>
-      <SearchLabel>Pesquisar por nome</SearchLabel>
+      <SearchLabel htmlFor={inputId}>Pesquisar por nome</SearchLabel>
       <SearchBarWrapper>
-        <SearchInput type="text" value={value} onChange={onChange} />
+        <SearchInput
+          id={inputId}
+          name="searchName"
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
         <SearchIcon icon="material-symbols:search-rounded" />
       </SearchBarWrapper>
     </SearchBarContainer>
