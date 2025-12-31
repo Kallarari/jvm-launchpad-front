1af63e3c35bf567a3b8e171396abb11df550723c
Mariana <mariana.iwaivk@gmail.com>
2025-12-31 10:51:25 -0300

    refactor(auth): update AuthContext, useAuth hook and PrivateRoute logic

--- src/squad-4-src/context/AuthContext.tsx
+++ src/squad-4-src/context/AuthContext.tsx
@@ -1,9 +1,11 @@
-import { createContext, useContext, type ReactNode } from "react";
+import { createContext, type ReactNode } from "react";
 import { usersMock } from "../mocks/users.mock";
 import type { User } from "../interfaces/user";
 import type { AuthContextType } from "./interface";
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const firstAdmin: User | null =
@@ -14,10 +16,4 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       {children}
     </AuthContext.Provider>
   );
-};
-
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) throw new Error("useAuth must be used within an AuthProvider");
-  return context;
 };
